443d3af134a55f9bc82ac16b6dcee0b67f505490
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-04-03 15:24:50 +0200

    pycodestyle

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -3,26 +3,27 @@
 
 from base_caching import BaseCaching
 
+
 class FIFOCache(BaseCaching):
-  """class"""
-  def __init__(self):
-    """initialization"""
-    super().__init__()
+    """class"""
+    def __init__(self):
+        """initialization"""
+        super().__init__()
 
-  def put(self, key, item):
-    """method"""
-    if key or item is not None:    
-      if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-        first_key = next(iter(self.cache_data))
-        print("DISCARD:", first_key)
-        del self.cache_data[first_key]
-      
-      self.cache_data[key] = item
+    def put(self, key, item):
+        """method"""
+        if key or item is not None:
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_key = next(iter(self.cache_data))
+                print("DISCARD:", first_key)
+                del self.cache_data[first_key]
 
-def get(self, key):
-        """"Must return the value in 
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Must return the value in 
         self.cache_data linked to key."""
         if key:
             return self.cache_data.get(key)
         else:
-          return None
+            return None
