4f6c37f8e3e179fd773e67150197f856d4718494
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-04-03 10:23:04 +0200

    Create 0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """A class that inherits from BaseCaching
+    and is a caching system:"""
+    def put(self, key, item):
+        """Must assign to the dictionary"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """"Must return the value in 
+        self.cache_data linked to key."""
+        if key:
+            return self.cache_data.get(key)
