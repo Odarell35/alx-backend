a835b65c83171613d01674729b379f9d284c3923
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-04-03 12:06:27 +0200

    pycodestyle

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -21,8 +21,7 @@ class LIFOCache(BaseCaching):
             del self.cache_data[last_key]
         self.cache_data[key] = item
 
-   def get(self, key):
-     """"Must return the value in
-     self.cache_data linked to key."""
-     if key:
-       return self.cache_data.get(key) 
+    def get(self, key):
+        """ Must return the value in self.cache_data linked to key """
+        if key:
+            return self.cache_data.get(key)
