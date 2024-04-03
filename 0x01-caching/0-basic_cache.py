04617e4050d3682e1707f2a2dd6c32bb10f11e55
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-04-03 10:26:18 +0200

    pycodestyle

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -12,7 +12,7 @@ class BasicCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """"Must return the value in 
+        """"Must return the value in
         self.cache_data linked to key."""
         if key:
             return self.cache_data.get(key)
