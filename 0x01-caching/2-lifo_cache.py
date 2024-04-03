27dc8c922efb8d5c6f1fb2a5bab69bb08616c4d6
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-04-03 15:55:54 +0200

    Update 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Module"""
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
@@ -9,6 +9,7 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """ Initialize the LIFO cache """
         super().__init__()
+        self.order = []
 
     def put(self, key, item):
         """ Add item to the cache """
@@ -16,12 +17,17 @@ class LIFOCache(BaseCaching):
             return
 
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            last_key = list(self.cache_data.keys())[-1]
+            last_key = self.order.pop()
             print("DISCARD:", last_key)
             del self.cache_data[last_key]
+
         self.cache_data[key] = item
+        self.order.append(key)
 
     def get(self, key):
         """ Must return the value in self.cache_data linked to key """
-        if key:
-            return self.cache_data.get(key)
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
