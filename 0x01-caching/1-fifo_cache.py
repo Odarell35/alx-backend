efd44ec137eb3f976405204a76e709b9170bafb9
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-04-03 11:27:57 +0200

    Create 1-fifo_cache.py

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Module"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+  """class"""
+  def __init__(self):
+    """initialization"""
+    super().__init__()
+
+  def put(self, key, item):
+    """method"""
+    if key or item is None:
+      return
+      
+    if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+      first_key = next(iter(self.cache_data))
+      print("DISCARD:", first_key)
+      del self.cache_data[first_key]
+      
+      self.cache_data[key] = item
+
+def get(self, key):
+        """"Must return the value in 
+        self.cache_data linked to key."""
+        if key:
+            return self.cache_data.get(key)
