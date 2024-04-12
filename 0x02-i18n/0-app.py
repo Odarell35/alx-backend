50f037b1dbcf1e91664518762a0fbdd2ebde3420
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-04-12 18:06:59 +0200

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 """ 0-app module """
-from flask import Flask, render_template
+from flask import Flask
+from routes.routes_0 import app_routes
 
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def home():
-    return render_template('0-index.html')
+app.register_blueprint(app_routes)
 
 
 if __name__ == "__main__":
-    """run app"""
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="5000")
