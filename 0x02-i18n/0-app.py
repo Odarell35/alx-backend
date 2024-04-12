e6250e02cad2c6af4b778121afc3f907e9860dcb
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-04-12 18:01:05 +0200

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """ 0-app module """
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
-app.register_blueprint()
 
 
 @app.route('/')
